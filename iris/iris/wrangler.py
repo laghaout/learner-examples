c48f803e667d9e9d98c73c256dd16b912c7e834c
ala-csis <ala@csis.dk>
2023-06-13 22:10:16 +0200

    Applied the standard folder structure for packaging iris

--- iris/iris/wrangler.py
+++ iris/iris/wrangler.py
@@ -10,41 +10,39 @@ import pandas as pd
 import learners.wrangler as wra
 import learners.utilities as util
 
+
 class Wrangler(wra.Wrangler):
-    
     def acquire(self):
-        
         super().acquire()
-        
+
         self.dataset = pd.read_csv(self.data_source, delimiter=self.delimiter)
-        
-        print(self.dataset.head())  
+
+        print(self.dataset.head())
 
     def __call__(self):
-        """ Consolidate the fine-grained beliefs into a scalar target. """
-        
+        """Consolidate the fine-grained beliefs into a scalar target."""
+
         report = super().__call__()
-        
-        self.datasets['raw'] = self.dataset.copy()       
-        
+
+        self.datasets["raw"] = self.dataset.copy()
+
         self.split()
-        
+
         from sklearn.preprocessing import LabelBinarizer
+
         self.encoder = LabelBinarizer()
-        self.encoder.fit(list(set(self.dataset['train'][self.target_name])))
-        
+        self.encoder.fit(list(set(self.dataset["train"][self.target_name])))
+
         return report
 
-    def explore(self, part: str='train'):
-        
+    def explore(self, part: str = "train"):
         report = super().explore()
-        
-        print(self.dataset[part].info())
-        
-        return report
-        
-    def split(self, split_sizes=None):
 
+        print(self.dataset[part].info())
+
+        return report
+
+    def split(self, split_sizes=None):
         super().split(split_sizes)
 
         # Split all the recorded datasets.
@@ -53,17 +51,16 @@ class Wrangler(wra.Wrangler):
                 self.datasets[k] = util.split(self.datasets[k], self.parts)
 
         # Split the main dataset.
-        self.dataset = util.split(self.dataset, self.parts)         
-        
+        self.dataset = util.split(self.dataset, self.parts)
+
+
 class OneOff:
-
     def __init__(self, **kwargs):
-
         import seaborn as sns
         import learners.utilities as util
-        
+
         util.args_to_attributes(self, **kwargs)
-        
-        iris = sns.load_dataset('iris')
+
+        iris = sns.load_dataset("iris")
         iris = iris.sample(frac=1, random_state=41)
-        iris.to_csv('data/data.csv', sep='\t', index=False)      
+        iris.to_csv("data/data.csv", sep="\t", index=False)
